76103247dc78c395bd42d0d74d30d029d252b4d4
Anand Hegde <ahegde@akamai.com>
2021-08-27 23:36:34 +0530

    Sort arrays, then find common prefix in first and last elements

---
+++ arrays/14LongestPrefix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        Arrays.sort(strs);
+        
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        
+        int c=0;
+        while(c<first.length() && first.charAt(c)==last.charAt(c))
+            c++;
+        
+        if(c==0)
+            return "";
+        
+        return first.substring(0,c);
+        
+        
+    }
+}
