d94246cdf3cb030d94e17c0c3ae8b55995b8ca1a
Anand Hegde <ahegde@akamai.com>
2022-01-31 19:11:01 +0530

    Return at the first instance of a map.contains

---
+++ hashmaps/217ContainsDuplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(freq.containsKey(nums[i]))
+                return true;
+            freq.put(nums[i], 1);
+        }
+        
+        return false;
+    }
+}
